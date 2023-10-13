877f6c78066fbb43c96a988e1d6f493d409ab4e5
Ameay Pande <ameay35@gmail.com>
2023-10-13 23:09:50 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./views/pages/login/Login";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
